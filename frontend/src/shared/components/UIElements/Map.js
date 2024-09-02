e127c79581c9d9304d86a07ce86946a6d22957b2
GAGANDASH002 <gagandash002@gmail.com>
2024-09-02 09:44:40 +0530

    rendering maps using Google Maps API

--- frontend/src/shared/components/UIElements/Map.js
+++ frontend/src/shared/components/UIElements/Map.js
@@ -1,66 +1,27 @@
 import React, { useRef, useEffect } from 'react';
-import 'ol/ol.css';
-import { Map as OlMap, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import { fromLonLat } from 'ol/proj';
-import Overlay from 'ol/Overlay';
 
 import './Map.css';
 
 const Map = props => {
   const mapRef = useRef();
+  
   const { center, zoom } = props;
-  const mapInstance = useRef(null);
-  const markerRef = useRef();
 
   useEffect(() => {
-    // If there's an existing map instance, set its target to null before creating a new one
-    if (mapInstance.current) {
-      mapInstance.current.setTarget(null);
-    }
-
-    // Create a new map instance and store it in the mapInstance ref
-    mapInstance.current = new OlMap({
-      target: mapRef.current,
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: fromLonLat([center.lng, center.lat]),
-        zoom: zoom,
-      }),
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom
     });
-
-    // Create a marker element
-    const marker = document.createElement('div');
-    marker.className = 'marker';
-    markerRef.current = new Overlay({
-      position: fromLonLat([center.lng, center.lat]),
-      positioning: 'center-center',
-      element: marker,
-      stopEvent: false,
-    });
-
-    // Add the marker overlay to the map
-    mapInstance.current.addOverlay(markerRef.current);
-
-    // Cleanup function to remove the map instance
-    return () => {
-      mapInstance.current.setTarget(null);
-    };
-  }, [center, zoom]);
+  
+    new window.google.maps.Marker({ position: center, map: map });
+  }, [center, zoom]);  
 
   return (
     <div
       ref={mapRef}
       className={`map ${props.className}`}
       style={props.style}
-      id="map"
-    >
-    </div>
+    ></div>
   );
 };
 
